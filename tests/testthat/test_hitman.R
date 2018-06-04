62c32ec2c0a4e7ba4bfb729ce29ca3b77b04b3b8
jonathan dreyfuss <jdreyf@gmail.com>
2018-06-04 19:03:03 -0400

    rm ezpreg

--- tests/testthat/test_hitman.R
+++ tests/testthat/test_hitman.R
@@ -2,16 +2,18 @@ context("hitman")
 
 test_that("E numeric", {
   ee <- rnorm(length(pheno.v))
-  hm <- hitman(E=ee, M=M, Y=pheno.v)
+  expect_message(hm <- hitman(E=ee, M=M, Y=pheno.v, verbose = TRUE))
   expect_lt(mean(hm$comb.p < 0.05), 0.1)
   
   covar.tmp <- rnorm(length(pheno.v))
   hm2 <- hitman(E=ee, M=M, Y=pheno.v, covariates=covar.tmp)
   expect_lte(mean(hm$comb.p==hm2[rownames(hm), "comb.p"]), 0.01)
+  
+  expect_error(hitman(E=numeric(length(pheno.v)), M=M, Y=pheno.v))
 })
 
 test_that("E binary", {
-  hm <- hitman(E=grp, M=M, Y=pheno.v)
+  expect_message(hm <- hitman(E=grp, M=M, Y=pheno.v, verbose = TRUE))
   expect_lt(mean(hm$comb.p < 0.05), 0.2)
   
   #same as factor but not numeric
@@ -21,6 +23,11 @@ test_that("E binary", {
   covar.tmp <- rnorm(length(pheno.v))
   hm3 <- hitman(E=grp, M=M, Y=pheno.v, covariates=covar.tmp)
   expect_lte(mean(hm$comb.p==hm3[rownames(hm), "comb.p"]), 0.01)
+  
+  #try to get ey.t=0, but EY.t~=1e-16
+  # y <- rep(1:3, times=2)
+  # limma_dep(object=y, Y=grp, prefix="EY")
+  # hm4 <- hitman(E=grp, M=M, Y=rep(1:3, times=2))
 })
 
 test_that("E nominal", {
@@ -36,6 +43,10 @@ test_that("E nominal", {
   covar.tmp <- rnorm(length(pheno.v))
   hm3 <- hitman(E=grp.tmp, M=M, Y=pheno.v, covariates=covar.tmp)
   expect_lte(mean(hm$comb.p==hm3[rownames(hm), "comb.p"]), 0.01)
+  
+  expect_error(hitman(E=rep("a", length(pheno.v)), M=M, Y=pheno.v))
+  expect_error(hitman(E=c(rep("a", length(pheno.v)-1), NA), M=M, Y=pheno.v))
+  expect_error(hitman(E=rep(NA, length(pheno.v)-1), M=M, Y=pheno.v))
 })
 
 test_that("gene1", {
