6b0cc77dc4ded190edeb4382c00f780b43b72759
jonathan dreyfuss <jdreyf@gmail.com>
2019-01-24 22:09:35 -0500

    update docs

--- R/limma_cor.R
+++ R/limma_cor.R
@@ -3,37 +3,34 @@
 #' Test correlation of each row of object to phenotype. By default, it uses the model 
 #' \code{design=model.matrix(~1+phenotype)} and tests 2nd coefficient.
 #'
-#' @param object A matrix-like data object containing log-ratios or 
-#'  log-expression values for a series of samples, with rows corresponding to 
-#'  genes and columns to samples.
+#' @param object A matrix-like data object containing log-ratios or log-expression values for a series of samples, with 
+#' rows corresponding to genes and columns to samples.
 #' @param phenotype Vector of phenotypes of the samples. Should be same length as
 #'  \code{ncol(object)}. If the vector is named, names should match 
 #'  \code{colnames(object)}.
-#' @param design the design matrix of the experiment, with rows corresponding to 
-#'  samples and columns to coefficients to be estimated. Can be used to provide 
-#'  covariates.
-#' @param prefix character string to add to beginning of column names.
-#' @param weights non-negative observation weights. Can be a numeric matrix of 
+#' @param design Design matrix of the experiment, with rows corresponding to samples and columns to coefficients to 
+#' be estimated. Can be used to provide covariates.
+#' @param prefix Character string to add to beginning of column names.
+#' @param weights Non-negative observation weights. Can be a numeric matrix of 
 #'  individual weights, of same size as the object expression matrix, or a 
 #'  numeric vector of array weights with length equal to \code{ncol} of the 
 #'  expression matrix, or a numeric vector of gene weights with length equal to 
 #'  \code{nrow} of the expression matrix. Set to \code{NULL} to ignore \code{object$weights}. \code{weights=NA} 
 #'   (with length one) doesn't pass weights to \code{limma}.
-#' @param trend logical, should an intensity-trend be allowed for the prior 
+#' @param trend Logical, should an intensity-trend be allowed for the prior 
 #'  variance? Default is that the prior variance is constant.
-#' @param adjust.method method used to adjust the p-values for multiple testing.
+#' @param adjust.method Method used to adjust the p-values for multiple testing.
 #' @param coef Integer coefficient of the linear model to test; passed to \code{\link[ezlimma]{eztoptab}}.
 #' @param reorder.rows logical, should rows be reordered by F-statistic from 
 #'  \code{\link[limma]{toptable}} or be left in the same order as 
 #'  \code{object}?
 #' @param reduce.df Number of degrees of freedom to subtract from residual. This may be necessary if
-#' \code{\link[limma]{removeBatchEffect}} was previously applied to \code{object}. Must be <= df.residual returned by
+#' \code{\link[limma]{removeBatchEffect}} was previously applied to \code{object}. Must be <= \code{df.residual} returned by
 #' \code{\link[limma]{lmFit}}.
 #' @param check_names Logical indicating if \code{names(phenotype)=rownames(object)} should be checked.
-#' @param cols columns of \code{topTable} output the user would like in the 
-#'  result. Some column names, such as \code{adj.P.Val} are changed. If \code{logFC}
-#'  is specified, \code{FC} will also be given.
-#' @return Dataframe.
+#' @param cols Columns of \code{\link[limma]{topTable}} output the user would like in the result. Some column names
+#' are changed, such as \code{adj.P.Val} -->  \code{FDR} and \code{logFC} --> \code{slope}.
+#' @return Data frame.
 #' @details Exactly one of \code{design} or \code{phenotype} must be non-null. If \code{design} is \code{NULL} and \code{phenotype} 
 #' is given, design will be calculated as \code{model.matrix(~0+phenotype)}. See further details in \code{\link[limma]{lmFit}}.
 #' @seealso \code{\link[limma]{lmFit}} and \code{\link[limma]{eBayes}}.
@@ -74,7 +71,7 @@ limma_cor <- function(object, phenotype=NULL, design=NULL, prefix=NULL, weights=
   fit2 <- limma::eBayes(fit, trend=trend)
   res.mat <- eztoptab(fit2, coef=coef, cols=cols, adjust.method=adjust.method)
   
-  #change logFC to coeff and get rid of FC
+  #change logFC to slope and get rid of FC
   colnames(res.mat) <- gsub('logFC', 'slope', colnames(res.mat))
   res.mat <- res.mat[,setdiff(colnames(res.mat), 'FC')]
   
