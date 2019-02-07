a18e55776dbb19bf97c8ebb9030de6a05b9464f8
jonathan dreyfuss <jdreyf@gmail.com>
2019-02-07 16:33:28 -0500

    rm ::: from tests

--- tests/testthat/test_top_xl.R
+++ tests/testthat/test_top_xl.R
@@ -1,7 +1,7 @@
 context("top xl")
 
 test_that("returned df", {
-  tx1 <- ezlimma:::top_xl(pwy.tab=rcn.f, n.toptabs = 1)
+  tx1 <- top_xl(pwy.tab=rcn.f, n.toptabs = 1)
   expect_equal(grep("=HYPERLINK(", tx1$xl[,1], fixed = TRUE), 1)
   expect_equal(tx1$pwy.csv.nms, "pwy1")
 })
--- tests/testthat/test_write_top_xl.R
+++ tests/testthat/test_write_top_xl.R
@@ -1,20 +1,20 @@
 context("write top xl")
 
 test_that("returned df", {
-  wtx <- ezlimma:::write_top_xl(pwy.tab=rcn.f, feat.lst=fl, feat.tab=eztt)
+  wtx <- write_top_xl(pwy.tab=rcn.f, feat.lst=fl, feat.tab=eztt)
   
   expect_equal(grep("=HYPERLINK(", wtx[,1], fixed = TRUE), 1:nrow(wtx))
   expect_equal(wtx[,-1], rcn.f)
   expect_equal(as.character(wtx[1,1]), '=HYPERLINK("pathways/pwy1.csv","pwy1")')
   
-  wtx1 <- ezlimma:::write_top_xl(pwy.tab=rcn.f, feat.lst=fl, feat.tab=eztt, n.toptabs = 1)
+  wtx1 <- write_top_xl(pwy.tab=rcn.f, feat.lst=fl, feat.tab=eztt, n.toptabs = 1)
   expect_equal(grep("=HYPERLINK(", wtx1[,1], fixed = TRUE), 1)
 })
 
 test_that("write out file", {
   unlink("text_wtx", recursive = TRUE) #in case it already exists
   
-  ezlimma:::write_top_xl(pwy.tab=rcn.f, feat.lst=fl, feat.tab=eztt, name="text_wtx", n.toptabs = 1)
+  write_top_xl(pwy.tab=rcn.f, feat.lst=fl, feat.tab=eztt, name="text_wtx", n.toptabs = 1)
   expect_true(file.exists("text_wtx/text_wtx.xlsx"))
   expect_true(file.exists("text_wtx/pathways/pwy1.csv"))
   expect_false(file.exists("text_wtx/pathways/pwy2.csv"))
