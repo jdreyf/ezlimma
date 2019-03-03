7a174fa5828c856cfa60152ede6beeb6b7ed339e
jonathan dreyfuss <jdreyf@gmail.com>
2019-03-03 18:46:44 -0500

    multicor

--- R/multi_cor.R
+++ R/multi_cor.R
@@ -8,18 +8,19 @@
 #' @param method Character string indicating which association is to be used 
 #'   for the test. One of \code{"pearson"}, \code{"spearman"}, \code{"kendall"}, 
 #'   from \code{\link[stats]{cor.test}} or \code{"limma"} for \code{\link{limma_cor}}.
-#' @param check.names Logical; should \code{names(pheno.tab)=rownames(object)} be checked?
+#' @param check.names Logical; should \code{rownames(pheno.tab)=colnames(object)} be checked?
 #' @param limma.cols If \code{method="limma"}, \code{cols} from \code{\link{limma_cor}} to include.
 #' @param covariates If \code{method="limma"}, numeric vector or matrix of covariates to include in 
 #' \code{\link{limma_cor}} \code{design} matrix.
 #' @inheritParams limma_contrasts
 #' @inheritParams limma_cor
 #' @return Data frame with several statistical columns corresponding to each phenotype and one row per feature.
-#' @details Each column of \code{pheno.tab} is tested independently.
+#' @details Each column of \code{pheno.tab} is tested independently. Arguments \code{covariates}, \code{block}, and
+#' \code{correlation} only apply if \code{method="limma"}.
 #' @export
 
 multi_cor <- function(object, pheno.tab, method=c("pearson", "spearman", "kendall", "limma"), reorder.rows=TRUE, 
-                      prefix=NULL, adjust.method="BH", covariates=NULL, check.names=TRUE,
+                      prefix=NULL, block=NULL, correlation=NULL, adjust.method="BH", covariates=NULL, check.names=TRUE,
                      limma.cols=c("AveExpr", "P.Value", "adj.P.Val", "logFC")){
   method <- match.arg(method)
   if (is.null(dim(pheno.tab))) stop("pheno.tab needs to have rows and columns.")
@@ -33,13 +34,17 @@ multi_cor <- function(object, pheno.tab, method=c("pearson", "spearman", "kendal
     prefix.tmp <- ifelse(!is.null(prefix), paste(prefix, colnames(pheno.tab)[ind], sep="."), colnames(pheno.tab)[ind])
     if (method=="limma"){
       if (is.null(covariates)){
-        cor.tmp <- data.matrix(limma_cor(object, pheno.tab[,ind], reorder.rows=FALSE, prefix=prefix.tmp, cols=limma.cols))
+        cor.tmp <- data.matrix(limma_cor(object, pheno.tab[,ind], reorder.rows=FALSE, prefix=prefix.tmp, block = block,
+                                         correlation = correlation, cols=limma.cols))
       } else {
         des.tmp <- stats::model.matrix(~1+pheno.tab[,ind]+covariates)
-        cor.tmp <- data.matrix(limma_cor(object, design=des.tmp, reorder.rows=FALSE, prefix=prefix.tmp, cols=limma.cols))
+        cor.tmp <- data.matrix(limma_cor(object, design=des.tmp, reorder.rows=FALSE, prefix=prefix.tmp, block = block,
+                                         correlation = correlation, cols=limma.cols))
       }
     } else {
-      if (!is.null(covariates)) warning("method is not limma, so covariates argument ignored.")
+      if (!is.null(covariates) || !is.null(block) || !is.null(correlation)){
+        warning("method is not limma, so limma-specific arguments ignored.")
+      }
       cor.tmp <- ezcor(object, pheno.tab[,ind], method=method, reorder.rows=FALSE, prefix=prefix.tmp, adjust.method=adjust.method)
     }
     if (!is.null(cor.mat)){ stopifnot(rownames(cor.mat)==rownames(cor.tmp)) }
