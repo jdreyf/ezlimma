0beae65045c2987700c7c9117aa758fee3bc7289
jonathan dreyfuss <jdreyf@gmail.com>
2020-10-28 21:31:11 -0400

    Update fc2ratio.R

--- R/fc2ratio.R
+++ R/fc2ratio.R
@@ -1,6 +1,6 @@
 #' Transform fold-change (FC) into a ratio
 #'
-#' Transform fold-change (FC) into a ratio. It's assumed \pkg{ezlimma} tranformed a ratio less than 1 into
+#' Transform fold-change (FC) into a ratio. It's assumed \pkg{ezlimma} transformed a ratio less than 1 into
 #' a negative FC, e.g. a ratio of 0.5 gave a FC of -2. This function reverses that operation.
 #'
 #' @param FC Numeric vector of fold-changes.
@@ -9,7 +9,7 @@
 
 fc2ratio <- function(FC){
   ret <- rep(NA, length(FC))
-  fc.pos <- which(FC > 0)
+  fc.pos <- which(FC >= 0)
   if (length(fc.pos) > 0) ret[fc.pos] <- FC[fc.pos]-1
   fc.neg <- which(FC < 0)
   if (length(fc.neg) > 0) ret[fc.neg] <- -1*(1 - 1/abs(FC[fc.neg]))
