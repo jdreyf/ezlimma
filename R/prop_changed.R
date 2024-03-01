65bd0d64c73671a4316206c0056587d2403dc8ca
jonathan dreyfuss <jdreyf@gmail.com>
2024-03-01 16:01:15 -0500

    rm test that wasn't necessary in prop_changed

--- R/prop_changed.R
+++ R/prop_changed.R
@@ -5,11 +5,10 @@
 #' @param feat.tab Table of feature (e.g. gene) statistics.
 #' @param feat.lst Named list, with each element containing names or indices of rows of \code{feat.tab}.
 #' @param stat.col Column name or column index with signed statistics.
-#' @param p.col Column name or column indix with p-values.
+#' @param p.col Column name or column index with p-values.
 
 prop_changed <- function(feat.tab, feat.lst, stat.col=1, p.col=2){
-  stopifnot(feat.tab[,p.col]>=0, feat.tab[,p.col]<=1, ncol(feat.tab) > 1, nrow(feat.tab) > 1, 
-            any(feat.tab[,stat.col] < 0))
+  stopifnot(feat.tab[,p.col]>=0, feat.tab[,p.col]<=1, ncol(feat.tab) > 1, nrow(feat.tab) > 1)
   prop.tab <- t(sapply(feat.lst, FUN=function(x){
     sig <- feat.tab[x, p.col] <= 0.05
     prop.up <- mean(sig & feat.tab[x, stat.col] > 0)
